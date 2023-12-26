8ed170dfc8fd15dc2237a7a54d1f560ec055c315
Сартаев Рамазан Даулетович <@step.kz>
2023-12-26 21:12:53 +0600

    lesson_15

--- testapp/urls.py
+++ testapp/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
 
+from django.urls import path
 
+from .views import index
+
+app_name = 'testapp'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('test/', include("testapp.urls",namespace='test')),
-    path('', include('bboard.urls')),
+
+    path('', index, name = 'index'),
+
 
 ]
-

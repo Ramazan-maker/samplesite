b1a8e5f724cb43174ce8682d6ecd2ac4d306ea36
Сартаев Рамазан Даулетович <@step.kz>
2024-01-09 21:08:01 +0600

    lesson_17

--- bboard/views.py
+++ bboard/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanen
 from django.template import loader
 from django.template.loader import get_template, render_to_string
 from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 
@@ -29,16 +31,36 @@ def index(request):
 #     return render(request, 'by_rubric.html', context)
 
 
-class BbByRubricView(TemplateView):
+class BbByRubricView(ListView):
     template_name = 'by_rubric.html'
+    context_object_name = 'bbs'
+
+    def get_queryset(self):
+        return Bb.objects.filter(rubric=self.kwargs['rubric_id'])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['bbs'] = Bb.objects.filter(rubric=context['rubric_id'])
         context['rubrics'] = Rubric.objects.all()
-        context['current_rubric'] = Rubric.objects.get(pk=context['rubric_id'])
+        context['current_rubric'] = Rubric.objects.get(pk=self.kwargs['rubric_id'])
         return context
+# class BbByRubricView(TemplateView):
+#     template_name = 'by_rubric.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['bbs'] = Bb.objects.filter(rubric=context['rubric_id'])
+#         context['rubrics'] = Rubric.objects.all()
+#         context['current_rubric'] = Rubric.objects.get(pk=context['rubric_id'])
+#         return context
 
+
+class BbDetailView(DetailView):
+    model = Bb
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 
 # def add_and_save(request):
 #     print(request.headers['Accept-Encoding'])
@@ -74,3 +96,6 @@ class BbCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context['rubrics'] = Rubric.objects.annotate(cnt=Count('bb')).filter(cnt__gt=0)
         return context
+
+
+

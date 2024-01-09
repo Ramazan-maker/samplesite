6a2c9cf55587cc428c96c7309878b609b670c444
Сартаев Рамазан Даулетович <@step.kz>
2024-01-09 21:30:13 +0600

    lesson_17

--- bboard/urls.py
+++ bboard/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (index, BbByRubricView,
                     # by_rubric,
                     BbCreateView, #add_and_save
-                    BbDetailView
+                    BbDetailView,BbAddView
                         )
 
 
@@ -12,7 +12,9 @@ app_name = 'bboard'
 urlpatterns = [
     # path('add/', add_and_save, name='add'),
     path('detail/<int:pk>/', BbDetailView.as_view(), name='detail'),
-    path('add/', BbCreateView.as_view(), name='add'),
+    # path('add/', BbCreateView.as_view(), name='add'),
+    path('add/', BbAddView.as_view(), name='add'),
+
     # path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('<int:rubric_id>/', BbByRubricView.as_view(), name='by_rubric'),
 

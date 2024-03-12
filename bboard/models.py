b351d7366dc4ae02b5dfb2379d9475b966ec3c99
Ramazan-maker <ramazan.sartayev@mail.ru>
2024-03-12 11:50:50 +0500

    lesson26_homework28

--- bboard/models.py
+++ bboard/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.core import validators
 from django.core.exceptions import ValidationError
 from django.db import models
@@ -14,7 +16,8 @@ def validate_even(val):
     if val % 2 != 0:
         raise ValidationError('Число %(value)s нечётное',
                               code='odd',
-                              params={'value': val})
+                              params={'value': val}
+                              )
 
 
 class MinMaxValueValidator:
@@ -25,25 +28,18 @@ class MinMaxValueValidator:
     def __call__(self, val):
         if val < self.min_value or val > self.max_value:
             raise ValidationError(
-                'Введённое число должно находиться в диапазоне от %(min)s до %(max)s',
+                'Введенное число должно находиться в диапазоне от %(min)s до %(max)s',
                 code='out_of_range',
                 params={'min': self.min_value, 'max': self.max_value}
             )
 
 
 class Rubric(models.Model):
-    name = models.CharField(max_length=20, db_index=True, verbose_name='Название', unique=True)
-    # slug = models.SlugField(max_length=160, unique=True, verbose_name='Слаг')
+    name = models.CharField(max_length=20, db_index=True, verbose_name="Название", unique=True)
     order = models.SmallIntegerField(default=0, db_index=True)
 
     def __str__(self):
-        return self.name
-
-    # def save(self, *args, **kwargs):
-    #     super().save(*args, **kwargs)
-
-    # def delete(self, *args, **kwargs):
-    #     super().delete(*args, **kwargs)
+        return self.name #изменение rubric object 1 на недвижимость
 
     def get_absolute_url(self):
         return f"/{self.pk}/"
@@ -51,7 +47,7 @@ class Rubric(models.Model):
     class Meta:
         verbose_name_plural = 'Рубрики'
         verbose_name = 'Рубрика'
-        ordering = ['order', 'name']
+        ordering = ['order', 'name']  #сортировка по имени
 
 
 class Bb(models.Model):
@@ -62,32 +58,25 @@ class Bb(models.Model):
     )
 
     kind = models.CharField(max_length=1, choices=KINDS, default='s', verbose_name='Тип объявления')
-
-    rubric = models.ForeignKey("Rubric", null=True, on_delete=models.PROTECT,
-                               verbose_name='Рубрика')
+    rubric = models.ForeignKey("Rubric", null=True, on_delete=models.PROTECT, verbose_name="Рубрика")  #внешний ключ аргумент должен быть выше если без кавычек
     title = models.CharField(
+        # unique=True
         max_length=50,
         verbose_name="Товар",
         validators=[validators.RegexValidator(regex='^.{4,}$')],
-        error_messages={'invalid': 'Здесь могла быть ваша реклама!!!'}
-        # validators=[validators.MinLengthValidator(4),
-        #             validators.MaxLengthValidator(50)]
-    )
-    content = models.TextField(null=True, blank=True, verbose_name="Описание", default="Какое-то значение:")
-    price = models.DecimalField(
-        max_digits=8,
-        decimal_places=2,
-        verbose_name="Цена",
-        default=0,
-        # validators=[validators.MinValueValidator(0),
-        #             validators.MaxValueValidator(100500),
-        #             validators.DecimalValidator(8, 2)]
-        # validators=[validate_even,
-        #             MinMaxValueValidator(25, 45)]
-    )
+        error_messages={'invalid': 'Это мы сами написали'}
+      )
+
+    content = models.TextField(null=True, blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=8,
+                                decimal_places=2,
+                                verbose_name="Цена",
+                                default=0,
+                                )
     is_active = models.BooleanField(default=is_active_default)
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Опубликовано")
     updated = models.DateTimeField(auto_now=True, db_index=True, verbose_name="Изменено")
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return f'{self.title}'
@@ -98,6 +87,13 @@ class Bb(models.Model):
             errors['content'] = ValidationError('Укажите описание продаваемого товара')
         if self.price and self.price < 0:
             errors['price'] = ValidationError('Укажите неотрицательное значение цены')
+        if errors:
+            raise ValidationError(errors)
+
+    def clean_title(self):
+        errors = {}
+        if self.title == "Car":
+            errors['title'] = ValidationError('Бобры не продаются, родина в них нуждается')
         if errors:
             raise ValidationError(errors)
 
@@ -112,11 +108,10 @@ class Bb(models.Model):
         verbose_name_plural = 'Объявления'
         verbose_name = 'Объявление'
         ordering = ['-published', 'title']
-        # order_with_respect_to = 'rubric'
+        # order_with_respect_to = 'rubric' #возвращать отсортированный список с рубрикой
 
 
 class RevRubric(Rubric):
-
     class Meta:
         proxy = True
         ordering = ['-name']
--- testapp/models.py
+++ testapp/models.py
@@ -21,36 +21,89 @@ class Spare(models.Model):
 
 class Machine(models.Model):
     name = models.CharField(max_length=30)
-    spares = models.ManyToManyField(Spare, through='Kit',
-                                    through_fields=('machine', 'spare'))
+    spares = models.ManyToManyField(Spare, through='Kit', through_fields=('machine', 'spare'))
     notes = GenericRelation('Note')
+
     def __str__(self):
         return f'{self.name}'
+
+    # class Meta:
+    #     ordering = ['name']
 
 
 class Kit(models.Model):
     machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
     spare = models.ForeignKey(Spare, on_delete=models.CASCADE)
     count = models.PositiveIntegerField(default=0)
+
+
+# class AditMachine(Machine):
+#     car_model = models.CharField(max_length=30)
+#     car_year = models.PositiveIntegerField()
+#     car_make = models.CharField(max_length=30)
+#
+#     class Meta(Machine.Meta):
+#         abstract = True
+#
+#
+# # class OrdMachine(AditMachine):
+# #     class Meta(AditMachine.Meta):
+# #         proxy = True
+# #         ordering = ['car_year', 'car_make']
+
+
 class Note(models.Model):
     content = models.TextField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey(ct_field = 'content_type', fk_field = 'object_id')
+    content_object = GenericForeignKey(ct_field='content_type', fk_field='object_id')
+
 
 class Message(models.Model):
     content = models.TextField()
+
+
+class PrivateMessage(Message):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.OneToOneField(Message, on_delete=models.CASCADE, parent_link=True)
+
+
+# class Message(models.Model):
+#     content = models.TextField()
 #     name = models.CharField(max_length=20)
 #     email = models.EmailField()
 #
 #     class Meta:
 #         abstract = True
-#         ordering = ('name')
-class PrivateMessage(Message):
-    user =models.ForeignKey(User, on_delete=models.CASCADE)
-    message = models.OneToOneField(Message, on_delete=models.CASCADE, parent_link=True)
+#         ordering = ['name']
+#
+#
+# class PrivateMessage(Message):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
 #     name = models.CharField(max_length=40)
 #     email = None
 #
 #     class Meta(Message.Meta):
 #         pass
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    class Meta:
+        abstract = True
+        ordering = ['name']
+
+
+class Children(Person):
+    age = models.PositiveIntegerField()
+
+    class Meta(Person.Meta):
+        pass
+
+
+class Grandchildren(Children):
+    class Meta(Children.Meta):
+        proxy = True
+        ordering = ['age']
